716405f0d2f4446c33bd7205bc55c3ace49f8ec5
Abbas Borji <abbas_borji@outlook.com>
2023-05-22 13:48:35 +0300

    Insert & Get Data from Database using (Entity Framework)

---
+++ TaskTracker-Application/loginForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace TaskTracker_Application
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textUsername = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.groupBoxLogin = new System.Windows.Forms.GroupBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.usersDGV = new System.Windows.Forms.DataGridView();
+            this.groupBoxLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.usersDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textUsername
+            // 
+            this.textUsername.Location = new System.Drawing.Point(183, 84);
+            this.textUsername.Name = "textUsername";
+            this.textUsername.Size = new System.Drawing.Size(246, 22);
+            this.textUsername.TabIndex = 0;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(180, 54);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(73, 16);
+            this.labelUsername.TabIndex = 1;
+            this.labelUsername.Text = "Username:";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(257, 230);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(102, 39);
+            this.buttonLogin.TabIndex = 2;
+            this.buttonLogin.Text = "Sign In";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // groupBoxLogin
+            // 
+            this.groupBoxLogin.Controls.Add(this.usersDGV);
+            this.groupBoxLogin.Controls.Add(this.textBox5);
+            this.groupBoxLogin.Controls.Add(this.textBox4);
+            this.groupBoxLogin.Controls.Add(this.textBox3);
+            this.groupBoxLogin.Controls.Add(this.textBox2);
+            this.groupBoxLogin.Controls.Add(this.textBox1);
+            this.groupBoxLogin.Controls.Add(this.labelPassword);
+            this.groupBoxLogin.Controls.Add(this.textPassword);
+            this.groupBoxLogin.Controls.Add(this.labelUsername);
+            this.groupBoxLogin.Controls.Add(this.buttonLogin);
+            this.groupBoxLogin.Controls.Add(this.textUsername);
+            this.groupBoxLogin.Location = new System.Drawing.Point(28, 31);
+            this.groupBoxLogin.Name = "groupBoxLogin";
+            this.groupBoxLogin.Size = new System.Drawing.Size(612, 314);
+            this.groupBoxLogin.TabIndex = 3;
+            this.groupBoxLogin.TabStop = false;
+            this.groupBoxLogin.Text = "Login";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(180, 135);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(70, 16);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "Password:";
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(183, 165);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(246, 22);
+            this.textPassword.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(488, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(488, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(488, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(488, 165);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(488, 210);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // usersDGV
+            // 
+            this.usersDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.usersDGV.Location = new System.Drawing.Point(0, 164);
+            this.usersDGV.Name = "usersDGV";
+            this.usersDGV.RowHeadersWidth = 51;
+            this.usersDGV.RowTemplate.Height = 24;
+            this.usersDGV.Size = new System.Drawing.Size(240, 150);
+            this.usersDGV.TabIndex = 4;
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 375);
+            this.Controls.Add(this.groupBoxLogin);
+            this.Name = "loginForm";
+            this.Text = "PMIS";
+            this.Load += new System.EventHandler(this.loginForm_Load);
+            this.groupBoxLogin.ResumeLayout(false);
+            this.groupBoxLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.usersDGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textUsername;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.GroupBox groupBoxLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView usersDGV;
+    }
+}
+
