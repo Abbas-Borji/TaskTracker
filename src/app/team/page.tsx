149be16d9cb26171c1256b79aa81be9f15dbaf9c
Abbas Borji <abbas_borji@outlook.com>
2023-11-26 01:09:11 +0200

    Team Page

---
+++ src/app/team/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserLayout from "src/app/user-layout/layout";
+
+const TeamPage = () => {
+  return (
+    <>
+      <UserLayout />
+    </>
+  );
+};
+
+export default TeamPage;
