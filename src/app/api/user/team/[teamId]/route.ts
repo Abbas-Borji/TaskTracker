de39946779f68805001f7965df9e4d05193b4a22
Abbas Borji <abbas_borji@outlook.com>
2023-11-26 16:24:30 +0200

    Get Checklists Route Handler

---
+++ src/app/api/user/team/[teamId]/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+export async function GET(request: NextRequest) {
+  const url = request.nextUrl;
+  console.log('URL:', url.href); // Log the full URL
+
+  const teamId = url.pathname.split('/').pop(); // Extract the last segment of the pathname
+  console.log('Team ID:', teamId); // Log the extracted teamId
+
+  const prisma = new PrismaClient();
+
+  const assignments = await prisma.assignment.findMany({
+    where: {
+      teamId: Number(teamId),
+      employeeId: 1, // Constant user id for now, will be dynamic later based on Token
+    },
+  });
+
+  const checklistIds = assignments.map(assignment => assignment.checklistId);
+
+  const checklists = await prisma.checklist.findMany({
+    where: {
+      id: {
+        in: checklistIds,
+      },
+    },
+    include: {
+      manager: true,
+    },
+  });
+
+  const checklistNameWithManagerName = checklists.map(checklist => ({
+    checklist: {
+      id: checklist.id,
+      name: checklist.name,
+    },
+    manager: {
+      id: checklist.manager.id,
+      name: checklist.manager.fullName,
+    },
+  }));
+
+  return new NextResponse(JSON.stringify(checklistNameWithManagerName), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
