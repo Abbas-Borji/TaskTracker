c83ced4e08aeb5e1c136730a52135045ac864de6
Abbas Borji <abbas_borji@outlook.com>
2023-11-26 19:19:31 +0200

    Updated Seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,6 +7,7 @@ await prisma.user.createMany({
     data: [
       { fullName: "John Doe", email:"john@tasktracker.io", password:"123123123", role: "USER" },
       { fullName: "Jane Smith", email:"jane@tasktracker.io", password:"123123123", role: "MANAGER" },
+      { fullName: "Steve Jobs", email:"steve@tasktracker.io", password:"123123123", role: "MANAGER" },
       { fullName: "Alex Bjorn", email:"alex@tasktracker.io", password:"123123123", role: "ADMIN" },
     ],
     
@@ -17,7 +18,7 @@ async function seedTeams() {
   await prisma.team.createMany({
     data: [
       { name: "Team A", managerId: 2 },
-      { name: "Team B", managerId: 2 },
+      { name: "Team B", managerId: 3 },
     ],
   });
 }
@@ -26,7 +27,8 @@ async function seedChecklists() {
   await prisma.checklist.createMany({
     data: [
       { name: "Checklist 1", managerId: 2 },
-      { name: "Checklist 2", managerId: 2 },
+      { name: "Checklist 2", managerId: 3 },
+      { name: "Checklist 3", managerId: 4 },
     ],
   });
 }
@@ -35,8 +37,8 @@ async function seedQuestions() {
   await prisma.question.createMany({
     data: [
       { content: "Question 1?", checklistId: 1 },
-      { content: "Question 2?", checklistId: 1 },
-      { content: "Question 3?", checklistId: 2 },
+      { content: "Question 2?", checklistId: 2 },
+      { content: "Question 3?", checklistId: 3 },
     ],
   });
 }
@@ -49,6 +51,7 @@ async function seedOptions() {
       { content: "Option 3", questionId: 2 },
       { content: "Option 4", questionId: 2 },
       { content: "Option 5", questionId: 3 },
+      { content: "Option 6", questionId: 3 },
     ],
   });
 }
@@ -57,6 +60,8 @@ async function seedAssignments() {
   await prisma.assignment.createMany({
     data: [
       { employeeId: 1, checklistId: 1, teamId: 1, dueDate: new Date() },
+      { employeeId: 1, checklistId: 2, teamId: 1, dueDate: new Date() },
+      { employeeId: 1, checklistId: 3, teamId: 1, dueDate: new Date() },
       { employeeId: 2, checklistId: 2, teamId: 2, dueDate: new Date() },
       { employeeId: 2, checklistId: 1, teamId: 1, dueDate: new Date() },
     ],
@@ -77,8 +82,8 @@ async function seedFeedbacks() {
   await prisma.feedback.createMany({
     data: [
       { assignmentId: 1, content: "Feedback 1", managerId: 2 },
-      { assignmentId: 2, content: "Feedback 2", managerId: 2 },
-      { assignmentId: 3, content: "Feedback 3", managerId: 3 },
+      { assignmentId: 2, content: "Feedback 2", managerId: 3 },
+      { assignmentId: 3, content: "Feedback 3", managerId: 4 },
     ],
   });
 }
