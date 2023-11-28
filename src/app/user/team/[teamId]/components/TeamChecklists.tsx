8c9a48c3eada91a16c50d237af2d08261b3e2dad
Abbas Borji <abbas_borji@outlook.com>
2023-11-29 01:21:11 +0200

    Display Checklis Assignments + Display Submissions

--- src/app/user/team/[teamId]/components/TeamChecklists.tsx
+++ src/app/user/team/[teamId]/components/TeamChecklists.tsx
@@ -3,10 +3,16 @@ import React from "react";
 import { useEffect, useState } from "react";
 import ChecklistSkeletons from "./loading";
 
+interface Team {
+  name: string | null;
+}
+
 interface Checklist {
   info: {
     id: number;
     name: string;
+    dueDate: string;
+    viewed: boolean;
   };
   manager: {
     id: number;
@@ -14,17 +20,25 @@ interface Checklist {
   };
 }
 
-interface Manager {
-  id: number;
-  name: string;
+interface Submission {
+  id?: number;
+  status: string;
+  submittedAt?: string;
+  archivedByManager?: boolean;
+  checklistinfo?: {
+    name?: string;
+    managerName?: string;
+  };
 }
 
-type TeamChecklistsProps = {
+type ChecklistsProps = {
   teamId: number;
 };
 
-const TeamChecklists = ({ teamId }: TeamChecklistsProps) => {
+const Checklists = ({ teamId }: ChecklistsProps) => {
   const [checklists, setChecklists] = useState<Checklist[]>([]);
+  const [submissions, setSubmissions] = useState<Submission[]>([]);
+  const [team, setTeam] = useState<Team>({ name: null });
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -37,7 +51,12 @@ const TeamChecklists = ({ teamId }: TeamChecklistsProps) => {
           throw new Error("Failed to fetch");
         }
         const data = await response.json();
-        setChecklists(data);
+        const team = data.teamName;
+        const checklists = data.checklists;
+        const submissions = data.submissions;
+        setTeam(team);
+        setChecklists(checklists);
+        setSubmissions(submissions);
         setIsLoading(false);
       } catch (error) {
         console.error("Error fetching checklists:", error);
@@ -58,38 +77,101 @@ const TeamChecklists = ({ teamId }: TeamChecklistsProps) => {
 
   return (
     <div className="p-2">
-      <h1 className="mb-10 text-4xl font-medium">| Team {teamId}</h1>
+      <h1 className="mb-10 text-4xl font-medium">| {team.name}</h1>
       <div className="flex w-full flex-col gap-8 lg:flex-row">
-      <div className="w-full rounded-lg border border-gray-300 shadow-sm p-5 hover:shadow-md transition-shadow">
+        <div className="w-full rounded-lg border border-gray-300 p-5 shadow-sm transition-shadow hover:shadow-md">
           <h3 className="mb-5 text-xl font-medium">Pending Checklists</h3>
           <div>
-            {checklists.map((checklist: Checklist) => (
+            {checklists.slice(0, 3).map((checklist: Checklist) => (
               <div
                 key={checklist.info.id}
-                className="mb-4 rounded-lg bg-[#f5f5f5] p-4 shadow-md"
+                className="relative mb-4 rounded-lg bg-light p-4 shadow-md"
               >
-                <div className="text-lg font-bold">{checklist.info.name}</div>
-                <div className="text-gray-600">
-                  Assigned by: {checklist.manager.name}
+                <div className="flex items-start justify-between">
+                  <div>
+                    <div className="text-lg font-bold">
+                      {checklist.info.name}
+                    </div>
+                    <div className="mt-2 text-sm text-gray-600">
+                      Assigned by: {checklist.manager.name}
+                    </div>
+                  </div>
+                  <button
+                    className={`${
+                      checklist.info.viewed ? "bg-success" : "bg-primary"
+                    } h-8 w-20 rounded px-2 py-1 text-xs font-medium text-light`}
+                  >
+                    {checklist.info.viewed ? "Continue" : "Start"}
+                  </button>
+                </div>
+                {/* Date at the bottom */}
+                <div className="mt-2 text-sm italic text-gray-600">
+                  Due {checklist.info.dueDate}
                 </div>
               </div>
             ))}
+            <div className="mt-4 text-center">
+              <a
+                href="/path-to-all-assigned-checklists" // Replace with your target URL
+                className="text-blue-600 hover:text-blue-800"
+              >
+                View all
+              </a>
+            </div>
           </div>
         </div>
-        <div className="w-full rounded-lg border border-gray-300 shadow-sm p-5 hover:shadow-md transition-shadow">
+        <div className="w-full rounded-lg border border-gray-300 p-5 shadow-sm transition-shadow hover:shadow-md">
           <h3 className="mb-5 text-xl font-medium">Submitted Checklists</h3>
           <div>
-            {checklists.map((checklist: Checklist) => (
+            {submissions.slice(0, 3).map((submission) => (
               <div
-                key={checklist.info.id}
-                className="mb-4 rounded-lg bg-[#f5f5f5] p-4 shadow-md"
+                key={submission.id}
+                className="relative mb-4 rounded-lg bg-light p-4 shadow-md"
               >
-                <div className="text-lg font-bold">{checklist.info.name}</div>
-                <div className="text-gray-600">
-                  Assigned by: {checklist.manager.name}
+                {/* Ellipsis Icon */}
+                <div className="absolute right-6 top-2 rotate-90 transform cursor-pointer text-2xl text-gray-600 hover:text-gray-800">
+                  &#8942; {/* HTML entity for horizontal ellipsis */}
+                </div>
+                {submission.checklistinfo && (
+                  <div className="flex items-start justify-between">
+                    <div>
+                      <div className="flex items-center text-lg font-bold">
+                        {submission.checklistinfo.name}
+                        <span
+                          className={`ml-4 rounded px-2 py-1 text-xs ${
+                            submission.status === "PENDING"
+                              ? "bg-red-300"
+                              : submission.status === "OPENED"
+                                ? "bg-yellow-300"
+                                : submission.status === "REVIEWED"
+                                  ? "bg-green-300"
+                                  : ""
+                          }`}
+                        >
+                          {submission.status}
+                        </span>
+                      </div>
+                      <div className="mt-2 text-sm text-gray-600">
+                        Assigned by: {submission.checklistinfo.managerName}
+                      </div>
+                    </div>
+                  </div>
+                )}
+                {/* Date at the bottom */}
+                <div className="mt-2 text-sm italic text-gray-600">
+                  Submitted at {submission.submittedAt}
                 </div>
               </div>
             ))}
+
+            <div className="mt-4 text-center">
+              <a
+                href="/path-to-all-submitted-checklists" // Replace with your target URL
+                className="text-blue-600 hover:text-blue-800"
+              >
+                View all
+              </a>
+            </div>
           </div>
         </div>
       </div>
@@ -97,4 +179,4 @@ const TeamChecklists = ({ teamId }: TeamChecklistsProps) => {
   );
 };
 
-export default TeamChecklists;
+export default Checklists;
