f29ee1708ac7e3e21afd7d308652698d43dfc45e
Abbas Borji <abbas_borji@outlook.com>
2023-11-29 01:20:32 +0200

    Get Checklist Assignments Update + Get Submissions

--- src/app/api/user/team/[teamId]/route.ts
+++ src/app/api/user/team/[teamId]/route.ts
@@ -1,24 +1,100 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
 export async function GET(request: NextRequest) {
+  // Extract the teamId from the URL
   const url = request.nextUrl;
-  console.log('URL:', url.href); // Log the full URL
-
-  const teamId = url.pathname.split('/').pop(); // Extract the last segment of the pathname
-  console.log('Team ID:', teamId); // Log the extracted teamId
+  const teamId = url.pathname.split("/").pop();
 
   const prisma = new PrismaClient();
 
+  // Get the team name
+  const teamName = await prisma.team.findUnique({
+    where: {
+      id: Number(teamId),
+    },
+    select: {
+      name: true,
+    },
+  });
+
+  // Get all the assignments to a user in the specified team
   const assignments = await prisma.assignment.findMany({
     where: {
       teamId: Number(teamId),
       employeeId: 1, // Constant user id for now, will be dynamic later based on Token
     },
+    include: {
+      submission: true,
+      checklist: {
+        include: {
+          manager: true,
+        },
+      },
+    },
   });
 
-  const checklistIds = assignments.map(assignment => assignment.checklistId);
+  const submittedAtDates = assignments.map((assignment) => {
+    if (assignment.submission && assignment.submission.createdAt) {
+      const date = new Date(assignment.submission.createdAt);
+      return date.toLocaleDateString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+    }
+    return null; // Return null or a default value if submission or createdAt is not available
+  });
 
+  // Filter assignments to include only those with a submission
+  const filteredAssignments = assignments.filter(
+    (assignment) => assignment.submission,
+  );
+
+  // Then, map over the filtered assignments to structure your submissions data
+  const structuredSubmissions = filteredAssignments.map((assignment) => {
+    const submittedAt = assignment.submission?.createdAt
+      ? new Date(assignment.submission.createdAt).toLocaleDateString("en-US", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })
+      : null;
+
+    return {
+      id: assignment.submission?.id,
+      status: assignment.submission?.status,
+      submittedAt: submittedAt,
+      archivedByManager: assignment.submission?.archivedByManager,
+      checklistinfo: {
+        name: assignment.checklist.name,
+        managerName: assignment.checklist.manager.fullName,
+      },
+    };
+  });
+
+  // Get the due dates from the assignments
+  const dueDates = assignments.map((assignment) => {
+    const date = new Date(assignment.dueDate);
+    return date.toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  });
+
+  // Get the viewed status from the assignments
+  const assignmentViews = assignments.map(
+    (assignment) => assignment.viewedByEmployee,
+  );
+
+  // Get the checklist ids from the assignments
+  const checklistIds = assignments.map((assignment) => assignment.checklistId);
+
+  // Get the checklists including their managers using the checklist ids
   const checklists = await prisma.checklist.findMany({
     where: {
       id: {
@@ -30,10 +106,13 @@ export async function GET(request: NextRequest) {
     },
   });
 
-  const checklistNameWithManagerName = checklists.map(checklist => ({
+  // Structure the checklist data to be returned
+  const structuredchecklists = checklists.map((checklist, index) => ({
     info: {
       id: checklist.id,
       name: checklist.name,
+      dueDate: dueDates[index],
+      viewed: assignmentViews[index],
     },
     manager: {
       id: checklist.manager.id,
@@ -41,10 +120,18 @@ export async function GET(request: NextRequest) {
     },
   }));
 
-  return new NextResponse(JSON.stringify(checklistNameWithManagerName), {
+  // Add the team name to the final response
+  const finalResponse = {
+    teamName,
+    checklists: structuredchecklists,
+    submissions: structuredSubmissions,
+  };
+
+  // Return the final response
+  return new NextResponse(JSON.stringify(finalResponse), {
     status: 200,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   });
 }
