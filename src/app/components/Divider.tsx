6a7df0f8fdcae6b82eaed114a76aea010d4e8e6b
Abbas Borji <abbas_borji@outlook.com>
2023-12-03 18:10:17 +0200

    Divider Comment

--- src/app/components/Divider.tsx
+++ src/app/components/Divider.tsx
@@ -4,6 +4,7 @@ interface DividerProps {
   borderColor?: string;
 }
 
+// Divider with customizable border color
 const Divider = ({ borderColor = "gray-200" }: DividerProps) => {
   return <div className={"w-full border-t border-" + borderColor} />;
 };
