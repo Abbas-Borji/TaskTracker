32c991978a2db292f07e659a9714f520e442fb0d
Abbas Borji <abbas_borji@outlook.com>
2023-12-03 22:59:04 +0200

    Manager Layout

---
+++ src/app/manager/layout.tsx
@@ -0,0 +1,160 @@
+"use client";
+import React from "react";
+import { Fragment, useState } from "react";
+import { Dialog, Menu, Transition } from "@headlessui/react";
+import {
+  Bars3Icon,
+  BellIcon,
+  DocumentDuplicateIcon,
+} from "@heroicons/react/24/outline";
+import {
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/20/solid";
+import Sidebar from "../common/components/Sidebar";
+import classNames from "../common/functions/ClassNames";
+
+const tabs = [
+  {
+    name: "My Checklists",
+    href: "/manager/mychecklists",
+    icon: DocumentDuplicateIcon,
+    current: true,
+  },
+];
+
+const userNavigation = [
+  { name: "Your profile", href: "#" },
+  { name: "Sign out", href: "/auth/login" },
+];
+
+const ManagerNavigationLayout = ({ children }: { children: React.ReactNode }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <>
+      <div>
+        <Sidebar
+          tabs={tabs}
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+          userId={1}
+        />
+
+        <div className="xl:pl-72">
+          <div className="sticky top-0 z-40 xl:mx-auto xl:max-w-7xl xl:px-8">
+            <div className="flex h-16 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 xl:px-0 xl:shadow-none">
+              <button
+                type="button"
+                className="-m-2.5 p-2.5 text-gray-700 xl:hidden"
+                onClick={() => setSidebarOpen(true)}
+              >
+                <span className="sr-only">Open sidebar</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </button>
+
+              {/* Separator */}
+              <div
+                className="h-6 w-px bg-gray-200 xl:hidden"
+                aria-hidden="true"
+              />
+
+              <div className="flex flex-1 gap-x-4 self-stretch xl:gap-x-6">
+                <form className="relative flex flex-1" action="#" method="GET">
+                  <label htmlFor="search-field" className="sr-only">
+                    Search
+                  </label>
+                  <MagnifyingGlassIcon
+                    className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  <input
+                    id="search-field"
+                    className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
+                    placeholder="Search..."
+                    type="search"
+                    name="search"
+                  />
+                </form>
+                <div className="flex items-center gap-x-4 xl:gap-x-6">
+                  <button
+                    type="button"
+                    className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
+                  >
+                    <span className="sr-only">View notifications</span>
+                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+
+                  {/* Separator */}
+                  <div
+                    className="hidden xl:block xl:h-6 xl:w-px xl:bg-gray-200"
+                    aria-hidden="true"
+                  />
+
+                  {/* Profile dropdown */}
+                  <Menu as="div" className="relative">
+                    <Menu.Button className="-m-1.5 flex items-center p-1.5">
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full bg-gray-50"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                      <span className="hidden xl:flex xl:items-center">
+                        <span
+                          className="ml-4 text-sm font-semibold leading-6 text-gray-900"
+                          aria-hidden="true"
+                        >
+                          Tom Cook
+                        </span>
+                        <ChevronDownIcon
+                          className="ml-2 h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </Menu.Button>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                        {userNavigation.map((item) => (
+                          <Menu.Item key={item.name}>
+                            {({ active }) => (
+                              <a
+                                href={item.href}
+                                className={classNames(
+                                  active ? "bg-gray-50" : "",
+                                  "block px-3 py-1 text-sm leading-6 text-gray-900",
+                                )}
+                              >
+                                {item.name}
+                              </a>
+                            )}
+                          </Menu.Item>
+                        ))}
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <main className="py-10">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 xl:px-8">
+              {children}
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ManagerNavigationLayout;
