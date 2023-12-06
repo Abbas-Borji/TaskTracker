4844d73d1bb7be9f0ac7d23ea0651b7d6e013388
Abbas Borji <abbas_borji@outlook.com>
2023-12-06 13:56:17 +0200

    Test Continue with Google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
       <h1>TaskTracker</h1>
       <div className="grid grid-cols-2 gap-2">
         <Link
-          href="/auth/login"
+          href="/api/auth/signin"
           type="button"
           className="mt-8 rounded bg-[#F5F5F5] px-6 py-2 text-center text-xl font-semibold text-[#1F2937] shadow-sm hover:bg-indigo-900 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
         >
