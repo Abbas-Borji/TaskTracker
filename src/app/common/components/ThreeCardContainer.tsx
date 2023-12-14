6f0d9e6492937e1dd0c0d00481530311c6a71dbb
Abbas Borji <abbas_borji@outlook.com>
2023-12-14 22:59:50 +0200

    Added logical rendering

--- src/app/common/components/ThreeCardContainer.tsx
+++ src/app/common/components/ThreeCardContainer.tsx
@@ -16,17 +16,25 @@ const ThreeCardContainer = ({
   return (
     <div className="w-full rounded-lg border border-gray-300 p-5 shadow-sm transition-shadow hover:shadow-md">
       <h3 className="mb-5 text-xl font-medium">{title}</h3>
-      <div>
-        {items.slice(0, 3).map((item, index) => renderItem(item, index))}
-        <div className="mt-4 text-center">
-          <button
-            onClick={onViewAll} // Attach the onViewAll function to onClick event
-            className="text-blue-600 hover:text-blue-800"
-          >
-            View all
-          </button>
+      {items.length > 0 ? (
+        <div>
+          {items.slice(0, 3).map((item, index) => renderItem(item, index))}
+          {items.length > 3 && (
+            <div className="mt-4 text-center">
+              <button
+                onClick={onViewAll} // Attach the onViewAll function to onClick event
+                className="text-blue-600 hover:text-blue-800"
+              >
+                View all
+              </button>
+            </div>
+          )}
         </div>
-      </div>
+      ) : (
+        <div className="flex flex-grow items-center justify-center">
+          <p>No items found.</p>
+        </div>
+      )}
     </div>
   );
 };
