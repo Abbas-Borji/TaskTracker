bd9c23d3c59d73b36f9a6b0dd426bedd6af437c2
Abbas Borji <abbas_borji@outlook.com>
2023-12-16 21:54:52 +0200

    Updated admin general layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -9,12 +9,8 @@ import {
   DocumentCheckIcon,
   ChatBubbleLeftIcon,
   Bars3Icon,
-  ArrowPathIcon,
 } from "@heroicons/react/24/outline";
 import Sidebar from "../common/components/Sidebar";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import capitalizeFirstLetter from "../common/functions/CapitalizeFirstLetter";
 
 const tabs = [
   {
@@ -56,10 +52,6 @@ const tabs = [
 ];
 
 const AdminNavigationLayout = ({ children }: { children: React.ReactNode }) => {
-  const pathname = usePathname();
-  const segments = pathname.split("/");
-  const lastSegment: string = capitalizeFirstLetter(segments[segments.length - 1]);
-
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
@@ -72,48 +64,20 @@ const AdminNavigationLayout = ({ children }: { children: React.ReactNode }) => {
         />
 
         <div className="xl:pl-72">
-          <div className="sticky top-0 z-40 xl:mx-auto xl:max-w-7xl xl:px-8">
-            <div className="flex h-16 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 xl:px-0 xl:shadow-none">
+          <div className="sticky top-0 z-40 xl:mx-auto xl:hidden xl:max-w-7xl xl:px-8">
+            <div className="flex h-16 items-center gap-x-4 border-b border-light bg-dark px-4 shadow-sm sm:gap-x-6 sm:px-6 xl:px-0 xl:shadow-none">
               <button
                 type="button"
                 className="-m-2.5 p-2.5 text-gray-700 xl:hidden"
                 onClick={() => setSidebarOpen(true)}
               >
                 <span className="sr-only">Open sidebar</span>
-                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                <Bars3Icon className="h-6 w-6 text-light" aria-hidden="true" />
               </button>
 
               {/* Separator */}
-              <div
-                className="h-6 w-px bg-gray-200 xl:hidden"
-                aria-hidden="true"
-              />
-
-              {/* Flex container for items on the left */}
-              <div className="flex flex-1 items-center gap-x-4 xl:gap-x-6">
-                {/* Display last segment and Refresh button */}
-                <div className="flex items-center">
-                  <span className="mr-4 text-lg font-semibold">
-                    {lastSegment}
-                  </span>
-                  {/* Refresh button */}
-                  <button
-                    onClick={() => window.location.reload()}
-                    className="flex items-center justify-center rounded-lg bg-slate-600 p-1.5 text-white hover:bg-dark"
-                  >
-                    <ArrowPathIcon className="h-5 w-5" aria-hidden="true" />
-                    <span className="sr-only">Refresh</span>
-                  </button>
-                </div>
-              </div>
-
-              {/* Logout Button */}
-              <Link
-                href="/api/auth/signout"
-                className="flex w-24 justify-center rounded-md bg-red-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-900"
-              >
-                Logout
-              </Link>
+              <div className="h-6 w-px bg-light xl:hidden" aria-hidden="true" />
+              <h1 className="text-lg font-bold text-light">Dashboard</h1>
             </div>
           </div>
 
