ddf32857657d2f23b964d15591b13d3e3d9809bd
Abbas Borji <abbas_borji@outlook.com>
2023-12-18 12:40:45 +0200

    Admin dashboard submissions page

---
+++ src/app/admin/submissions/components/ActionButtons.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Link from "next/link";
+import { TrashIcon } from "@heroicons/react/24/outline";
+
+const ActionButtons = () => {
+  return (
+    <div className="flex">
+      <Link href={"#"} className="mr-4 px-2">
+        View
+      </Link>
+
+      <Link href={"#"}>
+        <TrashIcon className="h-5 w-5 text-gray-500" />
+      </Link>
+    </div>
+  );
+};
+
+export default ActionButtons;
