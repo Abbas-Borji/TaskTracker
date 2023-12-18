4230bf42befafce97939738895f3755d3ebdc78c
Abbas Borji <abbas_borji@outlook.com>
2023-12-18 09:42:16 +0200

    Admin checklists page

---
+++ src/app/admin/checklists/components/ActionButtons.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+import { TrashIcon } from "@heroicons/react/24/outline";
+
+const ActionButtons = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <Link href={"#"} className="mr-4 px-2">Edit</Link>
+      <Link href={"#"} className="mr-4 px-2">Assign</Link>
+
+      <Link href={"#"}>
+        <TrashIcon className="h-5 w-5 text-gray-500" />
+      </Link>
+    </div>
+  );
+};
+
+export default ActionButtons;
--- src/app/admin/checklists/page.tsx
+++ src/app/admin/checklists/page.tsx
@@ -1,9 +1,81 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
+import Dashboard from "../common/components/Dashboard";
+import Button from "@/app/common/components/Button";
+import ActionButtons from "./components/ActionButtons";
+import AllowOnlyAdmin from "@/app/common/functions/ClientAllowOnlyAdmin";
 
-const ChecklistsTable = () => {
-  return (
-    <div>ChecklistsTable</div>
-  )
+const columns = [
+  { title: "ID", dataKey: "id", sortable: true },
+  { title: "Name", dataKey: "name", sortable: true },
+  { title: "Manager", dataKey: "manager", sortable: true },
+  { title: "Total Questions", dataKey: "totalQuestions", sortable: true },
+  { title: "Actions", dataKey: "actions", render: ActionButtons },
+  // Add more columns if needed
+];
+
+interface User {
+  id?: string;
+  name: string;
 }
 
-export default ChecklistsTable
+interface responseChecklist {
+  id: number;
+  name: string;
+  manager: User;
+  totalQuestions: number;
+  questions: { id: number }[];
+}
+
+interface Checklist {
+  id: number;
+  name: string;
+  manager: string;
+  totalQuestions: number;
+}
+
+const ChecklistsTable = () => {
+  AllowOnlyAdmin();
+  const [checklists, setChecklists] = useState<Checklist[]>([]);
+
+  useEffect(() => {
+    const fetchChecklists = async () => {
+      try {
+        const response = await fetch(`/api/admin/checklists`);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const checklists: responseChecklist[] = await response.json();
+        const reshapedChecklists: Checklist[] = checklists.map((checklist) => ({
+          id: checklist.id,
+          name: checklist.name,
+          manager: checklist.manager.name,
+          totalQuestions: checklist.totalQuestions,
+        }));
+
+        setChecklists(reshapedChecklists);
+      } catch (error) {
+        console.error("Error fetching checklists:", error);
+      }
+    };
+
+    fetchChecklists();
+  }, []);
+
+  return (
+    <Dashboard
+      description="A list of all the users including their name, email, role, and department."
+      columns={columns}
+      data={checklists}
+      actionButton={
+        <Button
+          text="Create Team"
+          className="ml-auto bg-primary text-white hover:bg-indigo-400 focus-visible:outline-indigo-500"
+          onClick={() => console.log("Checklist created!")}
+        />
+      }
+    />
+  );
+};
+
+export default ChecklistsTable;
