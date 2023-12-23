f3927ed87f0ff8674e94a706e3ce4262ef125079
Abbas Borji <abbas_borji@outlook.com>
2023-12-24 00:09:53 +0200

    Manager Checklist Editing Page

--- src/app/manager/checklist/edit/page.tsx
+++ src/app/manager/checklist/edit/page.tsx
@@ -1,13 +1,52 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 import ChecklistForm from "@/app/common/components/ChecklistForm";
 import AllowOnlyManager from "@/app/common/functions/ClientAllowOnlyManager";
 import FormSkeleton from "@/app/common/components/FormSkeleton";
+import { Checklist } from "@/app/common/types/CreateOrEditChecklist";
 
-const ChecklistCreate = () => {
-  const [isLoading, setIsLoading] = useState(true);
+const ChecklistEdit = () => {
   AllowOnlyManager();
-  return <>{isLoading ? <FormSkeleton /> : <ChecklistForm />}</>;
+  const [checklist, setChecklist] = useState<Checklist | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const searchParams = useSearchParams();
+  const checklistId = Number(searchParams.get("checklistId"));
+
+  useEffect(() => {
+    const fetchChecklist = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(`/api/manager/checklists/get?checklistId=${checklistId}`);
+        if (response.ok) {
+          const data = await response.json();
+          setChecklist(data);
+        } else {
+          // Handle non-OK responses here
+          console.error('Failed to fetch checklist. HTTP status: ', response.status);
+        }
+      } catch (error) {
+        // Handle network errors or other exceptions here
+        console.error('An error occurred while fetching the checklist: ', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (checklistId) {
+      fetchChecklist();
+    }
+  }, [checklistId]);
+
+  return (
+    <>
+      {isLoading ? (
+        <FormSkeleton />
+      ) : (
+        checklist && <ChecklistForm initialChecklist={checklist} />
+      )}
+    </>
+  );
 };
 
-export default ChecklistCreate;
+export default ChecklistEdit;
