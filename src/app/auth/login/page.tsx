dbe49841f499f987e71ef1e722241a15efc3e987
Abbas Borji <abbas_borji@outlook.com>
2023-12-23 02:18:29 +0200

    Loading rendered in a different way

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -84,7 +84,6 @@ const Login = () => {
 
   return (
     <>
-      {isLoading ? <Loading /> : null}
       {isNotificationVisible ? (
         <Notification
           title={serverError}
@@ -94,156 +93,166 @@ const Login = () => {
         />
       ) : null}
       <Redirect />
-      <div className="flex h-full min-h-full flex-1 flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-          <div className="bg-white px-6 py-12 text-center shadow sm:rounded-lg sm:px-12">
-            <div className="mx-auto mb-6 inline-flex items-center gap-4">
-              <img className="h-16 w-auto" src={Logo.src} alt="Your Company" />
-              <h1 className="text-4xl text-gray-700">Log In</h1>
-            </div>
-            <form className="space-y-6" onSubmit={handleSubmit}>
-              <div>
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="email"
-                    className="block text-left text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Email address
-                  </label>
-                  {errors.email && (
-                    <p className="text-sm font-medium text-red-500">
-                      {errors.email}
-                    </p>
-                  )}
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    value={email} // Connect the input to the email state
-                    onChange={handleEmailChange} // Update state on change
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="flex h-full min-h-full flex-1 flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+            <div className="bg-white px-6 py-12 text-center shadow sm:rounded-lg sm:px-12">
+              <div className="mx-auto mb-6 inline-flex items-center gap-4">
+                <img
+                  className="h-16 w-auto"
+                  src={Logo.src}
+                  alt="Your Company"
+                />
+                <h1 className="text-4xl text-gray-700">Log In</h1>
               </div>
-
-              <div>
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="password"
-                    className="text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Password
-                  </label>
-                  {errors.password && (
-                    <p className="text-sm font-medium text-red-500">
-                      {errors.password}
-                    </p>
-                  )}
+              <form className="space-y-6" onSubmit={handleSubmit}>
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label
+                      htmlFor="email"
+                      className="block text-left text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Email address
+                    </label>
+                    {errors.email && (
+                      <p className="text-sm font-medium text-red-500">
+                        {errors.email}
+                      </p>
+                    )}
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      value={email} // Connect the input to the email state
+                      onChange={handleEmailChange} // Update state on change
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                 </div>
 
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    value={password} // Connect the input to the password state
-                    onChange={handlePasswordChange} // Update state on change
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign In
-                </button>
-              </div>
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label
+                      htmlFor="password"
+                      className="text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Password
+                    </label>
+                    {errors.password && (
+                      <p className="text-sm font-medium text-red-500">
+                        {errors.password}
+                      </p>
+                    )}
+                  </div>
 
-              <div className="flex items-center justify-between text-sm">
-                <div className="">
-                  <Link
-                    href="/auth/forget-password"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </Link>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      value={password} // Connect the input to the password state
+                      onChange={handlePasswordChange} // Update state on change
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="flex items-center justify-between text-sm">
-                <span className=" text-gray-900">
-                  Don't have an account?&nbsp;
-                  <Link
-                    href="/auth/signup"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
-                    Create One
-                  </Link>
-                </span>
-              </div>
-            </form>
+                    Sign In
+                  </button>
+                </div>
 
-            <div>
-              <div className="relative mt-8">
-                <div
-                  className="absolute inset-0 flex items-center"
-                  aria-hidden="true"
-                >
-                  <div className="w-full border-t border-gray-200" />
+                <div className="flex items-center justify-between text-sm">
+                  <div className="">
+                    <Link
+                      href="/auth/forget-password"
+                      className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
+                      Forgot password?
+                    </Link>
+                  </div>
                 </div>
-                <div className="relative flex justify-center text-sm font-medium leading-6">
-                  <span className="bg-white px-6 text-sm text-gray-900">
-                    OR
+                <div className="flex items-center justify-between text-sm">
+                  <span className=" text-gray-900">
+                    Don't have an account?&nbsp;
+                    <Link
+                      href="/auth/signup"
+                      className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
+                      Create One
+                    </Link>
                   </span>
                 </div>
-              </div>
-              <div className="mt-6">
-                <button
-                  className="mx-auto flex w-full items-center justify-center rounded-lg border-2 border-stone-300 bg-white py-2.5 text-center text-sm font-medium text-black hover:bg-white/90 focus:outline-none focus:ring-4 focus:ring-white/50"
-                  onClick={() => signIn("google", { callbackUrl: "/redirect" })}
-                >
-                  <svg
-                    className="mr-2 h-7 w-7"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="705.6"
-                    height="720"
-                    viewBox="0 0 186.69 190.5"
+              </form>
+
+              <div>
+                <div className="relative mt-8">
+                  <div
+                    className="absolute inset-0 flex items-center"
+                    aria-hidden="true"
                   >
-                    <path
-                      fill="#4285f4"
-                      d="M-1089.333-687.239v36.888h51.262c-2.251 11.863-9.006 21.908-19.137 28.662l30.913 23.986c18.011-16.625 28.402-41.044 28.402-70.052 0-6.754-.606-13.249-1.732-19.483z"
-                      transform="translate(1184.583 765.171)"
-                    ></path>
-                    <path
-                      fill="#34a853"
-                      d="M-1142.714-651.791l-6.972 5.337-24.679 19.223c15.673 31.086 47.796 52.561 85.03 52.561 25.717 0 47.278-8.486 63.038-23.033l-30.913-23.986c-8.486 5.715-19.31 9.179-32.125 9.179-24.765 0-45.806-16.712-53.34-39.226z"
-                      transform="translate(1184.583 765.171)"
-                    ></path>
-                    <path
-                      fill="#fbbc05"
-                      d="M-1174.365-712.61c-6.494 12.815-10.217 27.276-10.217 42.689s3.723 29.874 10.217 42.689c0 .086 31.693-24.592 31.693-24.592-1.905-5.715-3.031-11.776-3.031-18.098s1.126-12.383 3.031-18.098z"
-                      transform="translate(1184.583 765.171)"
-                    ></path>
-                    <path
-                      fill="#ea4335"
-                      d="M-1089.333-727.244c14.028 0 26.497 4.849 36.455 14.201l27.276-27.276c-16.539-15.413-38.013-24.852-63.731-24.852-37.234 0-69.359 21.388-85.032 52.561l31.692 24.592c7.533-22.514 28.575-39.226 53.34-39.226z"
-                      transform="translate(1184.583 765.171)"
-                    ></path>
-                  </svg>
-                  Continue with Google
-                </button>
+                    <div className="w-full border-t border-gray-200" />
+                  </div>
+                  <div className="relative flex justify-center text-sm font-medium leading-6">
+                    <span className="bg-white px-6 text-sm text-gray-900">
+                      OR
+                    </span>
+                  </div>
+                </div>
+                <div className="mt-6">
+                  <button
+                    className="mx-auto flex w-full items-center justify-center rounded-lg border-2 border-stone-300 bg-white py-2.5 text-center text-sm font-medium text-black hover:bg-white/90 focus:outline-none focus:ring-4 focus:ring-white/50"
+                    onClick={() =>
+                      signIn("google", { callbackUrl: "/redirect" })
+                    }
+                  >
+                    <svg
+                      className="mr-2 h-7 w-7"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="705.6"
+                      height="720"
+                      viewBox="0 0 186.69 190.5"
+                    >
+                      <path
+                        fill="#4285f4"
+                        d="M-1089.333-687.239v36.888h51.262c-2.251 11.863-9.006 21.908-19.137 28.662l30.913 23.986c18.011-16.625 28.402-41.044 28.402-70.052 0-6.754-.606-13.249-1.732-19.483z"
+                        transform="translate(1184.583 765.171)"
+                      ></path>
+                      <path
+                        fill="#34a853"
+                        d="M-1142.714-651.791l-6.972 5.337-24.679 19.223c15.673 31.086 47.796 52.561 85.03 52.561 25.717 0 47.278-8.486 63.038-23.033l-30.913-23.986c-8.486 5.715-19.31 9.179-32.125 9.179-24.765 0-45.806-16.712-53.34-39.226z"
+                        transform="translate(1184.583 765.171)"
+                      ></path>
+                      <path
+                        fill="#fbbc05"
+                        d="M-1174.365-712.61c-6.494 12.815-10.217 27.276-10.217 42.689s3.723 29.874 10.217 42.689c0 .086 31.693-24.592 31.693-24.592-1.905-5.715-3.031-11.776-3.031-18.098s1.126-12.383 3.031-18.098z"
+                        transform="translate(1184.583 765.171)"
+                      ></path>
+                      <path
+                        fill="#ea4335"
+                        d="M-1089.333-727.244c14.028 0 26.497 4.849 36.455 14.201l27.276-27.276c-16.539-15.413-38.013-24.852-63.731-24.852-37.234 0-69.359 21.388-85.032 52.561l31.692 24.592c7.533-22.514 28.575-39.226 53.34-39.226z"
+                        transform="translate(1184.583 765.171)"
+                      ></path>
+                    </svg>
+                    Continue with Google
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
