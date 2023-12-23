06637e36fb5970a2b78d97925c5336d92fbcea8f
Abbas Borji <abbas_borji@outlook.com>
2023-12-24 00:12:53 +0200

    Admin get single checklist with all of its content api route handler

---
+++ src/app/api/admin/checklists/get/route.ts
@@ -0,0 +1,58 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "prisma/client";
+
+export async function GET(request: NextRequest) {
+  // Get the userId from the session
+  const session = await getServerSession(authOptions);
+  const userRole = session?.user?.role;
+
+  // Permission check to ensure only managers can access this route
+  if (userRole === "MANAGER" || userRole === "USER") {
+    return new NextResponse("Permission denied.", { status: 400 });
+  }
+
+  // Extract checklistId from the query parameter
+  const url = request.nextUrl;
+  const checklistIdQuery = url.searchParams.get("checklistId");
+  const checklistId = checklistIdQuery ? Number(checklistIdQuery) : null;
+
+  // Validate the checklistId
+  if (!checklistId) {
+    return new NextResponse(
+      JSON.stringify({ message: "Invalid checklistId." }),
+      {
+        status: 400,
+      },
+    );
+  }
+
+  // Check if the checklist exists
+  const checklist = await prisma.checklist.findUnique({
+    where: { id: checklistId },
+    include: {
+      questions: {
+        include: {
+          options: true,
+        },
+      },
+    },
+  });
+
+  if (!checklist) {
+    return new NextResponse(
+      JSON.stringify({ message: "Checklist not found." }),
+      {
+        status: 404,
+      },
+    );
+  }
+
+  return new NextResponse(JSON.stringify(checklist), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
