30c92a34ba0bf2d11b144257f4138041bb56b528
Abbas Borji <abbas_borji@outlook.com>
2023-12-31 23:20:39 +0200

    Bug fix: dropdown being hidden in a container with overflow y auto

--- src/app/common/components/DropdownMenu.tsx
+++ src/app/common/components/DropdownMenu.tsx
@@ -1,4 +1,6 @@
-import { Fragment } from "react";
+import ReactDOM from "react-dom";
+import { usePopper } from "react-popper";
+import { Fragment, useRef } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { EllipsisHorizontalIcon } from "@heroicons/react/20/solid";
 import classNames from "../functions/ClassNames";
@@ -25,6 +27,13 @@ export default function DropdownMenu({
   buttonClass,
   menuClass,
 }: DropdownMenuProps) {
+  const referenceElement = useRef(null);
+  const popperElement = useRef(null);
+  const { styles, attributes } = usePopper(
+    referenceElement.current,
+    popperElement.current,
+  );
+
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
@@ -34,58 +43,68 @@ export default function DropdownMenu({
             buttonClass ||
               "bg-gray-100 text-gray-400 hover:text-gray-600 focus:ring-indigo-500 focus:ring-offset-gray-100",
           )}
+          ref={referenceElement}
         >
           <span className="sr-only">Open options</span>
           <EllipsisHorizontalIcon className="h-5 w-5" aria-hidden="true" />
         </Menu.Button>
       </div>
 
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items
-          className={classNames(
-            "absolute right-0 z-10 mt-2 origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none",
-            menuClass || "w-56 bg-white",
-          )}
-        >
-          <div className="py-1">
-            {items.map((item, index) => (
-              <Menu.Item key={index}>
-                {({ active }) =>
-                  item.type === "link" && item.href ? (
-                    <Link
-                      href={item.href}
-                      className={classNames(
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                        "block px-4 py-2 text-sm",
-                      )}
-                    >
-                      {item.label}
-                    </Link>
-                  ) : (
-                    <button
-                      onClick={item.onClick}
-                      className={classNames(
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                        "block w-full px-4 py-2 text-left text-sm",
-                      )}
-                    >
-                      {item.label}
-                    </button>
-                  )
-                }
-              </Menu.Item>
-            ))}
-          </div>
-        </Menu.Items>
-      </Transition>
+      {ReactDOM.createPortal(
+        <div ref={popperElement} style={styles.popper} {...attributes.popper}>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              className={classNames(
+                "absolute right-0 z-10 mt-2 origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none",
+                menuClass || "w-56 bg-white",
+              )}
+            >
+              <div className="py-1">
+                {items.map((item, index) => (
+                  <Menu.Item key={index}>
+                    {({ active }) =>
+                      item.type === "link" && item.href ? (
+                        <Link
+                          href={item.href}
+                          className={classNames(
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block px-4 py-2 text-sm",
+                          )}
+                        >
+                          {item.label}
+                        </Link>
+                      ) : (
+                        <button
+                          onClick={item.onClick}
+                          className={classNames(
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-700",
+                            "block w-full px-4 py-2 text-left text-sm",
+                          )}
+                        >
+                          {item.label}
+                        </button>
+                      )
+                    }
+                  </Menu.Item>
+                ))}
+              </div>
+            </Menu.Items>
+          </Transition>
+        </div>,
+        document.body,
+      )}
     </Menu>
   );
 }
