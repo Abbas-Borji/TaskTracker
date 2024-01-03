8e2fb5bfa98e805adbfd9297a032bba0ceab6e29
Abbas Borji <abbas_borji@outlook.com>
2024-01-04 01:07:10 +0200

    Create feedback api route handler

---
+++ src/app/api/feedback/create/route.ts
@@ -0,0 +1,84 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
+
+interface FeedbackData {
+  submissionId: number;
+  feedbackContent: string;
+}
+
+export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  const userRole = session?.user?.role;
+  const userId = session?.user?.id;
+
+  // Allow only MANAGERs and ADMINs
+  if (userRole !== "MANAGER" && userRole !== "ADMIN") {
+    return new NextResponse("Permission denied.", { status: 403 });
+  }
+
+  // Parse request body
+  const { submissionId, feedbackContent }: FeedbackData = await request.json();
+
+  // Validate submission
+  const submission = await prisma.submission.findUnique({
+    where: { id: submissionId },
+  });
+  if (!submission) {
+    return new NextResponse(
+      JSON.stringify({ message: "Submission not found." }),
+      { status: 404 },
+    );
+  }
+
+  // Get assignment
+  const assignment = await prisma.assignment.findUnique({
+    where: { id: submission.assignmentId },
+    include: {
+      checklist: {
+        include: {
+          manager: true,
+        },
+      },
+    },
+  });
+
+  // Check if the user is the manager of the checklist
+  if (userRole === "MANAGER" && assignment?.checklist.manager.id !== userId) {
+    return new NextResponse("Permission denied.", { status: 403 });
+  }
+
+  // Check if the assignment already has feedback
+  const existingFeedback = await prisma.feedback.findFirst({
+    where: { assignmentId: submission.assignmentId },
+  });
+  if (existingFeedback) {
+    return new NextResponse(
+      JSON.stringify({ message: "Feedback already exists." }),
+      { status: 400 },
+    );
+  }
+
+  // Create feedback and update submission status
+  try {
+    console.log(feedbackContent);
+    const feedback = await prisma.feedback.create({
+      data: {
+        assignmentId: submission.assignmentId,
+        content: feedbackContent,
+        managerId: userId!,
+      },
+    });
+    await prisma.submission.update({
+      where: { id: submissionId },
+      data: { status: "REVIEWED" },
+    });
+    return new NextResponse(JSON.stringify(feedback), { status: 201 });
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: "Internal Server Error." }),
+      { status: 500 },
+    );
+  }
+}
