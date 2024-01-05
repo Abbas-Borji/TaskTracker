c691a1984d2cc2a9ac6dc939d9b23f3d1640f56c
Abbas Borji <abbas_borji@outlook.com>
2024-01-05 23:58:30 +0200

    Delete team api route handler

---
+++ src/app/api/team/delete/[teamId]/route.ts
@@ -0,0 +1,58 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { teamId: string } },
+) {
+  // Get the userRole from the session
+  const session = await getServerSession(authOptions);
+  const userRole = session?.user?.role;
+
+  // Extract teamId from URL
+  const teamId = params.teamId ? Number(params.teamId) : null;
+
+  // Permission check to ensure only admins can access this route
+  if (userRole !== "ADMIN") {
+    return new NextResponse("Permission denied.", { status: 400 });
+  }
+
+  // Validate the team
+  if (teamId) {
+    const teamExists = await prisma.team.findUnique({
+      where: { id: teamId },
+    });
+    if (!teamExists) {
+      return new NextResponse(JSON.stringify({ message: "Team not found." }), {
+        status: 404,
+      });
+    }
+    // Delete the team
+    try {
+      const team = await prisma.team.delete({
+        where: { id: teamId },
+      });
+
+      return new NextResponse(JSON.stringify(team), {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      return new NextResponse(
+        JSON.stringify({ message: "Internal Server Error." }),
+        { status: 500 },
+      );
+    }
+  } else {
+    return new NextResponse(
+      JSON.stringify({ message: "Team ID is not provided." }),
+      {
+        status: 400,
+      },
+    );
+  }
+}
