817646108c517c35071272ecd6bb3eb0f9a6c407
Abbas Borji <abbas_borji@outlook.com>
2024-01-16 23:06:20 +0200

    Updated signup zod schema to handle password confirmation errors

--- src/app/auth/signup/schema.ts
+++ src/app/auth/signup/schema.ts
@@ -1,11 +1,15 @@
 import { z } from "zod";
 
-const signUpSchema = z.object({
-  name: z.string().min(3, { message: "Invalid Name" }),
-  email: z.string().email({ message: "Invalid Email" }),
-  password: z
-    .string()
-    .min(6, { message: "Invalid Password" }),
-});
+const signUpSchema = z
+  .object({
+    name: z.string().min(3, { message: "Invalid Name" }),
+    email: z.string().email({ message: "Invalid Email" }),
+    password: z.string().min(6, { message: "Invalid Password" }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords must match",
+    path: ["confirmPassword"], // path of error
+  });
 
 export default signUpSchema;
