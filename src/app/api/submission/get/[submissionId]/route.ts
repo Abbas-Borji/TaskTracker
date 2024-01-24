9b82dffb0c3901ba4124de887e655980e6f14eb5
Abbas Borji <abbas_borji@outlook.com>
2024-01-24 23:25:26 +0200

    Fixed get submission api route handler

--- src/app/api/submission/get/[submissionId]/route.ts
+++ src/app/api/submission/get/[submissionId]/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
+import { getServerSessionUserInfo } from "@/app/common/functions/getServerSessionUserInfo";
 import prisma from "prisma/client";
 import {
   Question,
@@ -12,9 +11,8 @@ export async function GET(
   request: NextRequest,
   { params }: { params: { submissionId: string } },
 ) {
-  const session = await getServerSession(authOptions);
-  const userId = session?.user.id;
-  const userRole = session?.user.role;
+  const { userId, currentOrganization, userRole } =
+    await getServerSessionUserInfo();
 
   // Extract submission ID
   const submissionId = params.submissionId ? Number(params.submissionId) : null;
